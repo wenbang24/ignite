90c22396a0362d7bbe1461afc88253bbb33dcee4
wenbang24 <wenbangalt@gmail.com>
2025-11-25 19:36:20 +1100

    formatting + exporter

--- ignite.py
+++ ignite.py
@@ -1,33 +1,39 @@
-from flask import Flask, render_template, request, flash, redirect, url_for, send_from_directory
-import boto3
-from dotenv import load_dotenv
+import json
 import os
-from flask_admin.actions import action
-from flask_mongoengine import MongoEngine
-from flask_admin import Admin
-from flask_admin.contrib.mongoengine import ModelView
-from mongoengine import Document, connection
-from mongoengine.fields import (
-    EmailField,
-    ListField,
-    StringField,
-    SequenceField
+from datetime import datetime
+from urllib.parse import quote_plus
+
+import boto3
+import typesense
+from dotenv import load_dotenv
+from flask import (
+    Flask,
+    flash,
+    redirect,
+    render_template,
+    request,
+    send_from_directory,
+    url_for,
 )
+from flask_admin import Admin
+from flask_admin.actions import action
+from flask_admin.contrib.mongoengine import ModelView
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 from flask_login import (
     LoginManager,
     UserMixin,
-    login_user,
-    login_required,
-    logout_user,
     current_user,
+    login_required,
+    login_user,
+    logout_user,
 )
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_limiter import Limiter
-from flask_limiter.util import get_remote_address
-from urllib.parse import quote_plus
+from flask_mongoengine import MongoEngine
 from markupsafe import Markup
-from datetime import datetime
-import typesense
+from mongoengine import Document, connection
+from mongoengine.fields import EmailField, ListField, SequenceField, StringField
+from werkzeug.security import check_password_hash, generate_password_hash
+
 
 def upload_file(file, filename):
     s3_client = boto3.client(
@@ -54,7 +60,7 @@ def upload_file(file, filename):
 load_dotenv()
 app = Flask(__name__)
 app.config["MAX_CONTENT_LENGTH"] = 32 * 1000 * 1000
-#app.config['SERVER_NAME'] ="ignite-global.org"
+# app.config['SERVER_NAME'] ="ignite-global.org"
 app.secret_key = os.getenv("SECRET_KEY")
 
 limiter = Limiter(
@@ -65,38 +71,34 @@ limiter = Limiter(
 )
 
 uri = os.getenv("DB_URI")
-app.config['MONGODB_SETTINGS'] = {
-    'db': 'test',
-    'host': uri
-}
+app.config["MONGODB_SETTINGS"] = {"db": "test", "host": uri}
 db = MongoEngine(app)
 
-client = typesense.Client({
-    'nodes': [{
-        'host': 'localhost',
-        'port': '8108',
-        'protocol': 'http'
-    }],
-    'api_key': os.getenv('TYPESENSE_API_KEY'),
-    'connection_timeout_seconds': 2
-})
+client = typesense.Client(
+    {
+        "nodes": [{"host": "localhost", "port": "8108", "protocol": "http"}],
+        "api_key": os.getenv("TYPESENSE_API_KEY"),
+        "connection_timeout_seconds": 2,
+    }
+)
 artwork_schema = {
-    'name': 'artworksearch',
-    'fields': [
-        {'name': 'name', 'type': 'string'},
-        {'name': 'country', 'type': 'string', 'facet': True},
-        {'name': 'artname', 'type': 'string'},
-        {'name': 'medium', 'type': 'string', 'facet': True},
-        {'name': 'caption', 'type': 'string'}
-    ]
+    "name": "artworksearch",
+    "fields": [
+        {"name": "name", "type": "string"},
+        {"name": "country", "type": "string", "facet": True},
+        {"name": "artname", "type": "string"},
+        {"name": "medium", "type": "string", "facet": True},
+        {"name": "caption", "type": "string"},
+    ],
 }
-#client.collections['artworksearch'].delete()
-#client.collections.create(artwork_schema)
-#artworkcursor = connection.get_db().display_artwork.watch()
+# client.collections['artworksearch'].delete()
+# client.collections.create(artwork_schema)
+# artworkcursor = connection.get_db().display_artwork.watch()
 
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
+
 
 class User(Document, UserMixin):
     name = StringField(required=True)
@@ -105,6 +107,7 @@ class User(Document, UserMixin):
 
     def get_id(self):
         return self.email
+
 
 class Artwork(Document):
     name = StringField(required=True)
@@ -115,6 +118,7 @@ class Artwork(Document):
     medium = StringField(required=True)
     caption = StringField(required=True)
     filename = StringField(required=True)
+
 
 class DisplayArtwork(Document):
     id = SequenceField(primary_key=True)
@@ -127,6 +131,7 @@ class DisplayArtwork(Document):
     caption = StringField(required=True)
     filename = StringField(required=True)
     votes = ListField(EmailField())
+
 
 class Message(Document):
     name = StringField(required=True)
@@ -149,7 +154,7 @@ s3 = boto3.client(
 )
 response = s3.list_buckets()
 for bucket in response["Buckets"]:
-    print(f'Bucket:  {bucket["Name"]}')
+    print(f"Bucket:  {bucket['Name']}")
 
 ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif", "webp"}
 
@@ -163,13 +168,20 @@ def home():
 def gallery():
     if "q" in request.args:
         query = request.args["q"]
-        search_results = client.collections['artworksearch'].documents.search({
-            'q': query,
-            'query_by': 'name,country,artname,medium,caption'
-        })
+        search_results = client.collections["artworksearch"].documents.search(
+            {"q": query, "query_by": "name,country,artname,medium,caption"}
+        )
         results = []
-        for hit in search_results['hits']:
-            results.append(DisplayArtwork.objects(name=hit['document']['name'], country=hit['document']['country'], artname=hit['document']['artname'], medium=hit['document']['medium'], caption=hit['document']['caption']).first())
+        for hit in search_results["hits"]:
+            results.append(
+                DisplayArtwork.objects(
+                    name=hit["document"]["name"],
+                    country=hit["document"]["country"],
+                    artname=hit["document"]["artname"],
+                    medium=hit["document"]["medium"],
+                    caption=hit["document"]["caption"],
+                ).first()
+            )
         return render_template("gallery.html", artworks=results)
     return render_template("gallery.html", artworks=DisplayArtwork.objects)
 
@@ -178,13 +190,16 @@ def gallery():
 def contact():
     return render_template("contact.html")
 
+
 @app.route("/about")
 def about():
     return render_template("about.html")
 
+
 @app.route("/winners")
 def winners():
     return render_template("winners.html")
+
 
 @app.route("/contact", methods=["POST"])
 @limiter.limit(
@@ -193,9 +208,9 @@ def winners():
 )
 def contact_post():
     newMessage = Message(
-        name = request.form["name"],
-        email = request.form["email"],
-        message = request.form["message"]
+        name=request.form["name"],
+        email=request.form["email"],
+        message=request.form["message"],
     )
     newMessage.save()
     flash("Message sent successfully")
@@ -210,9 +225,11 @@ def donate():
 def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
 @app.route("/submit", methods=["GET"])
 def submit():
     return render_template("submit.html")
+
 
 @app.route("/submit", methods=["POST"])
 @limiter.limit(
@@ -236,7 +253,9 @@ def submit_post():
             + "."
             + file.filename.rsplit(".", 1)[1].lower()
         )
-        if Artwork.objects(filename=filename, email=current_user.get_id()) or DisplayArtwork.objects(filename=filename, email=current_user.get_id()):
+        if Artwork.objects(
+            filename=filename, email=current_user.get_id()
+        ) or DisplayArtwork.objects(filename=filename, email=current_user.get_id()):
             flash(
                 "You have already submitted an artwork with this name; please contact our team if you believe this is a mistake or if you would like to modify it."
             )
@@ -268,30 +287,43 @@ def submit_post():
 
 
 admin_username = str(os.getenv("ADMIN_USERNAME"))
+
+
 class PendingArtworks(ModelView):
-    column_editable_list = ['name', 'artname', 'medium', 'caption']
-    column_searchable_list = ['name', 'parentname', 'email', 'country', 'artname', 'medium', 'caption']
+    column_editable_list = ["name", "artname", "medium", "caption"]
+    column_searchable_list = [
+        "name",
+        "parentname",
+        "email",
+        "country",
+        "artname",
+        "medium",
+        "caption",
+    ]
 
     def is_accessible(self):
         return current_user.is_authenticated and current_user.get_id() == admin_username
+
     def inaccessible_callback(self, name, **kwargs):
-        return redirect(url_for('login', next=request.url))
+        return redirect(url_for("login", next=request.url))
+
     def get_query(self):
         artworks = Artwork.objects
         return artworks
 
     def _list_thumbnail(view, context, model, name):
         if not model.filename:
-            return ''
+            return ""
 
         return Markup(
             f'<img src="https://ignite-global.s3.ap-southeast-2.amazonaws.com/{model.filename}" style="max-height: 200px;">'
         )
-    column_formatters = {
-        'filename': _list_thumbnail
-    }
 
-    @action('accept', 'Accept', 'Are you sure you want to accept the selected artworks?')
+    column_formatters = {"filename": _list_thumbnail}
+
+    @action(
+        "accept", "Accept", "Are you sure you want to accept the selected artworks?"
+    )
     def accept(self, ids):
         for _id in ids:
             artwork = Artwork.objects(id=_id).first()
@@ -306,103 +338,149 @@ class PendingArtworks(ModelView):
                 filename=artwork.filename,
             )
             newDisplayArtwork.save(force_insert=True)
-            client.collections['artworksearch'].documents.create({
-                'name': artwork.name,
-                'country': artwork.country,
-                'artname': artwork.artname,
-                'medium': artwork.medium,
-                'caption': artwork.caption
-            })
+            client.collections["artworksearch"].documents.create(
+                {
+                    "name": artwork.name,
+                    "country": artwork.country,
+                    "artname": artwork.artname,
+                    "medium": artwork.medium,
+                    "caption": artwork.caption,
+                }
+            )
             artwork.delete()
         flash("Artworks approved successfully")
-        return redirect(url_for('admin.index'))
+        return redirect(url_for("admin.index"))
 
-    @action('reject', 'Reject', 'Are you sure you want to delete the selected artworks?')
+    @action(
+        "reject", "Reject", "Are you sure you want to delete the selected artworks?"
+    )
     def reject(self, ids):
         for _id in ids:
             artwork = Artwork.objects(id=_id).first()
             artwork.delete()
-            s3.delete_object(
-                Bucket=os.getenv("AWS_BUCKET_NAME"),
-                Key=artwork.filename
-            )
+            s3.delete_object(Bucket=os.getenv("AWS_BUCKET_NAME"), Key=artwork.filename)
         flash("Artworks deleted successfully")
-        return redirect(url_for('admin.index'))
+        return redirect(url_for("admin.index"))
+
 
 class DisplayArtworks(ModelView):
-    column_editable_list = ['name', 'artname', 'medium', 'caption']
-    column_searchable_list = ['name', 'parentname', 'email', 'country', 'artname', 'medium', 'caption']
+    column_editable_list = ["name", "artname", "medium", "caption"]
+    column_searchable_list = [
+        "name",
+        "parentname",
+        "email",
+        "country",
+        "artname",
+        "medium",
+        "caption",
+    ]
 
     def is_accessible(self):
         return current_user.is_authenticated and current_user.get_id() == admin_username
 
     def inaccessible_callback(self, name, **kwargs):
-        return redirect(url_for('login', next=request.url))
+        return redirect(url_for("login", next=request.url))
 
     def get_query(self):
         artworks = DisplayArtwork.objects
         return artworks
+
     def _list_thumbnail(view, context, model, name):
         if not model.filename:
-            return ''
+            return ""
 
         return Markup(
             f'<img src="https://ignite-global.s3.ap-southeast-2.amazonaws.com/{model.filename}" style="max-height: 200px;">'
         )
+
     def _num_votes(view, context, model, name):
         return len(model.votes)
-    column_formatters = {
-        'filename': _list_thumbnail,
-        'votes': _num_votes
-    }
 
-    @action('remove', 'Remove', 'Are you sure you want to delete the selected artworks?')
+    column_formatters = {"filename": _list_thumbnail, "votes": _num_votes}
+
+    @action(
+        "remove", "Remove", "Are you sure you want to delete the selected artworks?"
+    )
     def reject(self, ids):
         for _id in ids:
             artwork = DisplayArtwork.objects(id=_id).first()
-            s3.delete_object(
-                Bucket=os.getenv("AWS_BUCKET_NAME"),
-                Key=artwork.filename
-            )
+            s3.delete_object(Bucket=os.getenv("AWS_BUCKET_NAME"), Key=artwork.filename)
             artwork.delete()
         flash("Artworks deleted successfully")
-        return redirect(url_for('admin.index'))
+        return redirect(url_for("admin.index"))
+
 
 class Messages(ModelView):
-    column_searchable_list = ['name', 'email', 'message']
+    column_searchable_list = ["name", "email", "message"]
 
     def is_accessible(self):
         print(current_user.get_id())
         return current_user.is_authenticated and current_user.get_id() == admin_username
 
     def inaccessible_callback(self, name, **kwargs):
-        return redirect(url_for('login', next=request.url))
+        return redirect(url_for("login", next=request.url))
 
     def get_query(self):
         messages = Message.objects
         return messages
 
+
 class Users(ModelView):
-    column_searchable_list = ['name', 'email']
-    column_exclude_list = ['password']
+    column_searchable_list = ["name", "email"]
+    column_exclude_list = ["password"]
 
     def is_accessible(self):
         print(current_user.get_id())
         return current_user.is_authenticated and current_user.get_id() == admin_username
 
     def inaccessible_callback(self, name, **kwargs):
-        return redirect(url_for('login', next=request.url))
+        return redirect(url_for("login", next=request.url))
 
     def get_query(self):
         users = User.objects
         return users
 
-app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
-admin = Admin(app, name='ignite', template_mode='bootstrap4')
+
+app.config["FLASK_ADMIN_SWATCH"] = "cerulean"
+admin = Admin(app, name="ignite", template_mode="bootstrap4")
 admin.add_view(PendingArtworks(Artwork))
 admin.add_view(DisplayArtworks(DisplayArtwork))
 admin.add_view(Messages(Message))
 admin.add_view(Users(User))
+
+
+@app.route("/admin/export", methods=["GET"])
+@login_required
+def export_display_artworks():
+    if current_user.get_id() != admin_username:
+        return "Unauthorized", 403
+
+    export = []
+    for artwork in DisplayArtwork.objects:
+        export.append(
+            {
+                "id": artwork.id,
+                "name": artwork.name,
+                "parentname": artwork.parentname,
+                "email": artwork.email,
+                "country": artwork.country,
+                "artname": artwork.artname,
+                "medium": artwork.medium,
+                "caption": artwork.caption,
+                "filename": artwork.filename,
+                "vote_count": len(artwork.votes),
+            }
+        )
+
+    response = app.response_class(
+        response=json.dumps(export, ensure_ascii=False, indent=2),
+        mimetype="application/json",
+    )
+    response.headers["Content-Disposition"] = (
+        "attachment; filename=display_artworks.json"
+    )
+    return response
+
 
 @app.route("/upvote", methods=["POST"])
 def upvote():
@@ -422,12 +500,13 @@ def upvote():
 
 @app.route("/login", methods=["GET", "POST"])
 @limiter.limit(
-    "10/second", error_message="You have sent too many requests. Please try again later."
+    "10/second",
+    error_message="You have sent too many requests. Please try again later.",
 )
 def login():
     dest = request.args.get("next")
     if request.method == "POST":
-        user = User.objects(email=request.form['email']).first()
+        user = User.objects(email=request.form["email"]).first()
         print(type(user))
         if user and check_password_hash(user.password, request.form["password"]):
             login_user(user)
@@ -473,15 +552,17 @@ def logout():
     flash("Logged out successfully")
     return redirect(url_for("home"))
 
+
 @app.route("/legal")
 def legal():
     return render_template("legal.html")
 
-@app.route('/robots.txt')
-@app.route('/sitemap.xml')
+
+@app.route("/robots.txt")
+@app.route("/sitemap.xml")
 def static_from_root():
     return send_from_directory(app.static_folder, request.path[1:])
 
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
-
